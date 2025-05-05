daf4b7f63e73b4102ea45cf9374b804b2809927a
jelo <jelo@bioxtorm.com>
2025-05-05 08:42:04 +0100

    add: autoload script

--- addons/milestone/milestone.gd
+++ addons/milestone/milestone.gd
@@ -2,25 +2,23 @@
 extends EditorPlugin
 
 const MILESTONE_PANEL = preload("res://addons/milestone/components/milestone_panel.tscn")
-var panel
+
+var milestone_view
 
 
 func _enable_plugin() -> void:
-	# Add autoloads here.
-	pass
+	add_autoload_singleton("MILESTONE", "res://addons/milestone/autoload/milestone.gd")
 
 
 func _disable_plugin() -> void:
-	# Remove autoloads here.
-	pass
-
+	remove_autoload_singleton("MILESTONE")
 
 func _enter_tree() -> void:
-	# Initialization of the plugin goes here.
-	panel = MILESTONE_PANEL.instantiate()
-	add_control_to_container(EditorPlugin.CONTAINER_TOOLBAR, panel)
+	milestone_view = MILESTONE_PANEL.instantiate()
+	milestone_view.hide()
+	get_editor_interface().get_editor_main_screen().add_child(milestone_view)
 
 
 func _exit_tree() -> void:
-	remove_control_from_container(EditorPlugin.CONTAINER_TOOLBAR, panel)
-	panel.queue_free()
+	if milestone_view:
+		milestone_view.queue_free()
